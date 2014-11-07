75ce2210207e0561e2d9d8ccf1c93266379215c1
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-11-07 15:19:44 -0500

    works on soution

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -2,4 +2,19 @@ $(function(){
   var donutCounter = new DonutCounter();
 });
 
-// your code here
+function DonutCounter(){
+  this.$donutCount = $('#donuts h3 span');
+  this.$guestCount = $('#guests h3 span');
+  this.addListeners();
+}
+
+DonutCounter.prototype.addListeners = function() {
+  
+};
+
+DonutCounter.prototype.incrementerListeners = function() {
+  $('#donuts button:first-of-type, #guests button:first-of-type').click(function(){
+      var $span = $(this).parent().find("h3 span");
+      $span.text( Number($span.text())++ );
+    });
+};
