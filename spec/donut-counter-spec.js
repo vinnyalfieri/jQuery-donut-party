7e179f3a90b6674c563993d839b64133b72cee9b
Rose Weixel <roseweixel@users.noreply.github.com>
2015-07-20 11:43:17 -0400

    Add "guest-count" and "donut-count" classes to spans to make fixture match index.html

--- spec/donut-counter-spec.js
+++ spec/donut-counter-spec.js
@@ -1,6 +1,6 @@
 describe('DonutCounter', function() {
   beforeEach(function() {
-    setFixtures('<body><div class="wrapper"><h1>Donut Party!</h1><h2 id="status"></h2><div id="guests"><h3>Guests: <span>0</span></h3><button class="add">+</button><button class="remove">-</button></div><div id="donuts"><h3>Donuts: <span>0</span></h3><button class="add">+</button><button class="remove">-</button></div><div id="check"><button>Check</button></div></div></body>');
+    setFixtures('<body><div class="wrapper"><h1>Donut Party!</h1><h2 id="status"></h2><div id="guests"><h3>Guests: <span class="guest-count">0</span></h3><button class="add">+</button><button class="remove">-</button></div><div id="donuts"><h3>Donuts: <span class="donut-count">0</span></h3><button class="add">+</button><button class="remove">-</button></div><div id="check"><button>Check</button></div></div></body>');
     var donutCounter = new DonutCounter();
   });
 
